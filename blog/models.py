f745c8ce26d71f73fe8a30a2336e53e4ab802735
Junxian Qi <junxian.j.qi@gmail.com>
2021-01-15 17:53:04 +0800

    Add article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    # Article unique ID
+    article_id = models.AutoField(primary_key=True)
+    # Article title
+    title = models.TextField()
+    # Article brief content
+    brief_content = models.TextField()
+    # Article main content
+    content = models.TextField()
+    # Article publish date
+    publish_date = models.DateTimeField(auto_now=True)
