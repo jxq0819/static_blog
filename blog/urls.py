5efe3d5b5d26e3ff6cf42b733bc67326b283fac4
Junxian Qi <junxian.j.qi@gmail.com>
2021-01-16 15:51:38 +0800

    Add article content view and url

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ import blog.views
 
 urlpatterns = [
     path('hello_world', blog.views.hello_world),
+    path('content', blog.views.article_content),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
 
+from .models import Article
+
 
 def hello_world(request):
     return HttpResponse("Hello World")
+
+
+def article_content(request):
+    article = Article.objects.all()[0]
+    article_id = article.article_id
+    title = article.title
+    brief_content = article.brief_content
+    content = article.content
+    publish_date = article.publish_date
+    return_str = "title: %s, brief_content: %s, content: %s, article_id: %s, publish_date: %s" % (
+        title, brief_content, content, article_id, publish_date)
+
+    return HttpResponse(return_str)
